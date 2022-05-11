79aa35a01385cef5a6255410c720b6c4cb13e2a2
Kostya <rodina9180gaocheng@ukr.net>
2022-05-11 23:40:48 +0200

    completed adaptivity

--- js/main.js
+++ js/main.js
@@ -5,5 +5,14 @@ $(function(){
       variableWidth: true,
       prevArrow: '<img class="arrow arrow-left" src="images/arrow-left.svg" alt="arrow">',
       nextArrow: '<img class="arrow arrow-right" src="images/arrow-right.svg" alt="arrow">',
+      responsive: [
+         {
+            breakpoint: 361,
+            settings: {
+               variableWidth: false,
+               slidesToShow: 1
+            }
+         }
+      ]
    });
 });
